f9d7e5e883ef5468b3636041475552983a28b84c
Keith Howling <khowling@gmail.com>
2015-12-31 17:12:57 +0000

    h

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"usr strict"
+'use strict'
 
 var http = require('http');
 var ODataServer = require("simple-odata-server");
@@ -11,15 +11,14 @@ var model = {
             "id": {"type": "Edm.String", key: true},
             "customercardid": {"type": "Edm.String"},
             "companyid": {"type": "Edm.String"},
-            "storeid": {"type": "Edm.String"}
-        //    "totalamount": {"type": "Edm.Double"},
-      //      "enddatetime__c": {"type": "Edm.DateTimeOffset"}  //yyyy-MM-ddTHH:mm:ss.fffZ or yyyy-MM-ddTHH:mm:ss.fff[+&#124;-]HH:mm 2015-04-15T10:30:09.7550000Z
+            "storeid": {"type": "Edm.String"},
+            "totalamount": {"type": "Edm.Double"},
+            "enddatetime__c": {"type": "Edm.DateTimeOffset"}  //yyyy-MM-ddTHH:mm:ss.fffZ or yyyy-MM-ddTHH:mm:ss.fff[+&#124;-]HH:mm 2015-04-15T10:30:09.7550000Z
 				}
     },
     entitySets: {
         "slip": {
-          entityType: "jsreport.SlipType",
-        //    type: "jsreport.AccountType",
+          entityType: "jsreport.SlipType"
         }
     }
 };
@@ -35,16 +34,25 @@ console.log ('Starting odata server on : ' + process.env.ODATA_HOSTNAME);
 var odataServer = ODataServer(process.env.ODATA_HOSTNAME)
   .model(model)
   .query((setName, query, cb) => {
-
+		let client = new pg.Client(process.env.PG_URL);
 		console.log ('Connecting to (need to do it here because Azure cannot keep open a connection) : ' + process.env.PG_URL);
-		var client = new pg.Client(process.env.PG_URL);
+
 		client.connect(function(err) {
 		  if(err) {
 		    console.error('could not connect to postgres', err);
 				cb(err);
 		  } else {
-				console.log ("query with " + JSON.stringify(query));
-				client.query("SELECT id, customercardid, companyid, storeid from public.slip", function(err, result) {
+				let qstr = `SELECT ${Object.keys(query['$select']).join(',')} FROM  ${query['collection']}`;
+				if (query['$filter']) {
+					let first_field = Object.keys(query['$filter'])[0];
+					qstr+= ` WHERE ${first_field} = '${query['$filter'][first_field]}'`;
+				}
+				if (query['$limit']) {
+					qstr+= " limit " + query['$limit'];
+				}
+				console.log ("request with : " + JSON.stringify(query));
+				console.log ("query with : " + qstr);
+				client.query(qstr, function(err, result) {
 					client.end();
 					if (err) {
 						console.log ("query err " + JSON.stringify(err));
