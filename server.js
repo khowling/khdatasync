de1cf933bab2225a99a7adf9cdaded661a41ee7a
Keith Howling <khowling@gmail.com>
2015-12-31 17:43:59 +0000

    h

--- server.js
+++ server.js
@@ -13,7 +13,19 @@ var model = {
             "companyid": {"type": "Edm.String"},
             "storeid": {"type": "Edm.String"},
             "totalamount": {"type": "Edm.Double"},
-            "enddatetime": {"type": "Edm.DateTimeOffset"}  //yyyy-MM-ddTHH:mm:ss.fffZ or yyyy-MM-ddTHH:mm:ss.fff[+&#124;-]HH:mm 2015-04-15T10:30:09.7550000Z
+            "enddatetime": {"type": "Edm.DateTimeOffset"},  //yyyy-MM-ddTHH:mm:ss.fffZ or yyyy-MM-ddTHH:mm:ss.fff[+&#124;-]HH:mm 2015-04-15T10:30:09.7550000Z
+						"tsreceived": {"type": "Edm.DateTimeOffset"},
+					 "tsqueueinsert": {"type": "Edm.DateTimeOffset"},
+					 "tsworkerreceived": {"type": "Edm.DateTimeOffset"},
+					 "tsstartcrunsh": {"type": "Edm.DateTimeOffset"},
+					 "tsendcrunsh": {"type": "Edm.DateTimeOffset"},
+					 "tsdbinsert": {"type": "Edm.DateTimeOffset"},
+					 "tsreceivedstr": {"type": "Edm.String"},
+					 "tsworkerreceivedstr": {"type": "Edm.String"},
+					 "tsstartcrunchstr": {"type": "Edm.String"},
+					 "tsendcrunchstr": {"type": "Edm.String"},
+					 "tsdbinsertstr": {"type": "Edm.String"},
+					 "tsqueueinsertstr": {"type": "Edm.String"}
 				}
     },
     entitySets: {
